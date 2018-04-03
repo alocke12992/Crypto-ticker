a09224662fccc8087a86ddec37579bfe499e904d
Andrew Locke <alocke12992@gmail.com>
2018-04-03 14:01:58 -0600

    Adds loader

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -1,15 +1,16 @@
 class Api::CoinsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_coin, only: [:show, :update, :destroy]
+  before_action :set_coin, only: [:update, :destroy]
   BASE_URL = 'https://api.coinmarketcap.com/v1/ticker/'
 
   def index
     coins = HTTParty.get(BASE_URL)
     user_coins = current_user.coins
-    user_coins.each do |coin| 
-      res_coin = coins.find {|c| c['id'] == coin.cmc_id}
+    user_coins.each do |coin|
+      res_coin = coins.find { |c| c['id'] == coin.cmc_id }
       coin.update(price: res_coin['price_usd']) if res_coin
     end
+
     render json: current_user.coins
   end
 
@@ -22,19 +23,16 @@ class Api::CoinsController < ApplicationController
         user_id: current_user.id
       )
       watched.update(initial_price: coin.price) if watched.initial_price.nil?
-        render json: coin
+      render json: coin
     else
       render json: { errors: 'Coin Not Found' }, status: 422
     end
 
-    #find out if the coin exists on cmc
-    #create a coin in our database (or find it)
-    #create a watched_coin record
-    #render back the coin
   end
 
   def show
-    render json: @coin
+    res = HTTParty.get("#{BASE_URL}#{params[:id]}")
+    render json: res[0]
   end
 
   # PUT /api/coins/:id
@@ -48,4 +46,10 @@ class Api::CoinsController < ApplicationController
   def destroy
     @coin.destroy
   end
+
+  private
+    def set_coin
+      @coin = Coin.find(params[:id])
+    end
+
 end
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Coin from './Coin';
 import NoMatch from './NoMatch';
 import NavBar from './NavBar';
 import Login from './Login';
@@ -19,6 +20,7 @@ class App extends Component {
         <FetchUser>
           <Switch>
             <ProtectedRoute exact path='/' component={Home} />
+            <ProtectedRoute exact path='/coins/:id' component={Coin} />
             <AuthRoute exact path='/login' component={Login} />
             <AuthRoute exact path='/register' component={Register} />
             <Route component={NoMatch} />
